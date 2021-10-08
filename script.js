8289bb9f23c4b9678861f3d1ed58a52b5744e865
OurRichi <89989562+OurRichi@users.noreply.github.com>
2021-10-09 00:59:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function getAddress() {
     $.get("https://ipinfo.io", function(response) {
         var ip = response.ip;
 
-        if(ip !== "185.94.6.154") {
+        if(ip !== "--- (My IP Censored)") {
             window.location.replace("https://richi.ourcrew.eu/alpha-web/");
         }
     }, "json")
